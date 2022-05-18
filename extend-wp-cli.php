15f915f0c21158258f5bcd60f144c184725ea9b0
amy <amy.fiske@pantheon.io>
2022-05-17 18:59:41 -0700

    add extend cli function

--- extend-wp-cli.php
+++ extend-wp-cli.php
@@ -71,7 +71,16 @@ class extendWPCLI {
 
 
    public static function ewpc_add_package( $attributes ){
-    //your code here
+    if ( ! class_exists( 'WP_CLI' ) ) {
+        return;
+    }
+    
+    $wpcli_profile_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+    if ( file_exists( $wpcli_profile_autoloader ) ) {
+        require_once $wpcli_profile_autoloader;
+    }
+    
+    WP_CLI::add_command( 'profile', 'WP_CLI\Profile\Command' );
     }
 }
 
