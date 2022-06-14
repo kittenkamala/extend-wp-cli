e4552ca83810cb8480f96c3648ad1607bec75fbf
amy <amy.fiske@pantheon.io>
2022-06-13 18:19:42 -0700

    try writing to ~/.wp-cli/config.yml using put_contents

--- extend-wp-cli.php
+++ extend-wp-cli.php
@@ -75,7 +75,9 @@ class extendWPCLI {
     if ( ! class_exists( 'WP_CLI' ) ) {
         return;
     }
-    
+
+    echo file_put_contents("~/.wp-cli/config.yml","require: - pre-ssh.php");
+
     $wpcli_profile_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
     if ( file_exists( $wpcli_profile_autoloader ) ) {
         require_once $wpcli_profile_autoloader;
